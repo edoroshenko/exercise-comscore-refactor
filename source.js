3b1b5dbb7459b5f5fd49ae952461545160b0f10c
Evgeniy Doroshenko <esdoroshenko@esdoroshenko-osx.local>
2012-11-13 02:20:08 +0400

    code style fixing

--- source.js
+++ source.js
@@ -3,43 +3,42 @@ function calculatevalueandaddtohtmlobject (a, b, c) {
 	var tBody = this.domElement.querySelector(a);
 	var frag = document.createDocumentFragment().appendChild(tBody.cloneNode(false));
 
-	while(tBody.firstChild) {
+	while (tBody.firstChild) {
 		tBody.removeChild(tBody.firstChild);
 	}
 
 	// b holds the parameters for the calculation. The parameters
-	// are objects that have type, value, name, enum values
-	// the parameter type can be CSV_2_TO_10, CSV_2_TO_INF, CSV_1_TO_INF
-	// ENUM, BOOLEAN and COUNTRY
+	// are objects that have type, value, name, enum values.
+	// The parameter type can be CSV_2_TO_10, CSV_2_TO_INF, CSV_1_TO_INF, ENUM, BOOLEAN and COUNTRY
 	var parameters = b.parameters;
-	for(var i = 0, parameter; parameter = parameters[i++];) {
+	for (var i = 0, parameter; parameter = parameters[i++];) {
 		var value = parameter.value;
 		var csv = parameter.type == "CSV_2_TO_10" || parameter.type == "CSV_2_TO_INF" || parameter.type == "CSV_1_TO_INF";
 		if (csv) {
 			var chunks = value.split(",");
-			for (var i=0, len=chunks.length, chunk; i < len; i++) {
+			for (var i = 0, len = chunks.length, chunk; i < len; i++) {
 				chunk = chunks[i];
 				if (chunk.length > 50) {
 					if (text.length > 5) {
-						for (var j=0, len= chunk.length; j <= len; j += 5) {
+						for (var j = 0, len = chunk.length; j <= len; j += 5) {
 							wrapperText += chunk.substring(j, j + 5) + "\u200B";
 						}
 					} else {
-					wrapperText = chunk;
+						wrapperText = chunk;
 					}
 				}
 			}
 			value = chunks.join(", ");
 		}
-		if(parameter.type == "ENUM") {
-			if(parameter.enumValues[parameter.value-1]){
-				value = parameter.enumValues[parameter.value-1].displayName;
+		if (parameter.type == "ENUM") {
+			if (parameter.enumValues[parameter.value - 1]){
+				value = parameter.enumValues[parameter.value - 1].displayName;
 			} else {
 				value = "";
 			} 
-		} else if(parameter.type == "COUNTRY") {
+		} else if (parameter.type == "COUNTRY") {
 			value = getCountryDisplayNameByCode(parameter.value) || "";
-		} else if(parameter.type == "BOOLEAN") {
+		} else if (parameter.type == "BOOLEAN") {
 			value = (parameter.value && (parameter.value != "0") ? "%STATICTEXT_YES%" : "%STATICTEXT_NO%");
 		}
 
