dc23b43f3dbbfa71e3fdd42ec5c63834c17b8b83
Evgeniy Doroshenko <esdoroshenko@esdoroshenko-osx.local>
2012-11-13 11:11:03 +0400

    moving a formatting loop to a separate function; + Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Changes
+
+1. The "c" argument does not seem to be used. Removing it
+2. The contents of the loop through the parameters is self-contained and does not depend of aother parts of function. Also it provides a determined functional and calculates a concrete value. Moving it to a separate function getValueFormated.
--- source.js
+++ source.js
@@ -1,4 +1,4 @@
-function calculatevalueandaddtohtmlobject (a, b, c) {
+function calculatevalueandaddtohtmlobject (a, b) {
 	// a is a parent container that will serve to hold the calculated value
 	var tBody = this.domElement.querySelector(a);
 	var frag = document.createDocumentFragment().appendChild(tBody.cloneNode(false));
@@ -7,43 +7,53 @@ function calculatevalueandaddtohtmlobject (a, b, c) {
 		tBody.removeChild(tBody.firstChild);
 	}
 
-	// b holds the parameters for the calculation. The parameters
-	// are objects that have type, value, name, enum values.
+	// b holds the parameters for the calculation.
+	// The parameters are objects that have type, value, name, enum values.
 	// The parameter type can be CSV_2_TO_10, CSV_2_TO_INF, CSV_1_TO_INF, ENUM, BOOLEAN and COUNTRY
 	var parameters = b.parameters;
 	for (var i = 0, parameter; parameter = parameters[i++];) {
-		var value = parameter.value;
-		var csv = parameter.type == "CSV_2_TO_10" || parameter.type == "CSV_2_TO_INF" || parameter.type == "CSV_1_TO_INF";
-		if (csv) {
-			var chunks = value.split(",");
-			for (var i = 0, len = chunks.length, chunk; i < len; i++) {
-				chunk = chunks[i];
-				if (chunk.length > 50) {
-					if (text.length > 5) {
-						for (var j = 0, len = chunk.length; j <= len; j += 5) {
-							wrapperText += chunk.substring(j, j + 5) + "\u200B";
-						}
-					} else {
-						wrapperText = chunk;
-					}
-				}
-			}
-			value = chunks.join(", ");
-		}
-		if (parameter.type == "ENUM") {
-			if (parameter.enumValues[parameter.value - 1]){
-				value = parameter.enumValues[parameter.value - 1].displayName;
-			} else {
-				value = "";
-			} 
-		} else if (parameter.type == "COUNTRY") {
-			value = getCountryDisplayNameByCode(parameter.value) || "";
-		} else if (parameter.type == "BOOLEAN") {
-			value = (parameter.value && (parameter.value != "0") ? "%STATICTEXT_YES%" : "%STATICTEXT_NO%");
-		}
+		
 
 		frag.appendChild(ample.query("<tr/>").append(ample.query("<td/>").text((parameter.name || "").htmlSpecialChars())).append(ample.query("<td/>").text((value || "").htmlSpecialChars()))[0]);
 	};
 
 	tBody.parentNode.replaceChild(frag, tBody);
 };
+
+function getValueFormated(parameter) {
+	var value = parameter.value;
+	var csv = parameter.type == "CSV_2_TO_10" || parameter.type == "CSV_2_TO_INF" || parameter.type == "CSV_1_TO_INF";
+	if (csv) {
+		var chunks = value.split(",");
+		for (var i = 0, len = chunks.length, chunk; i < len; i++) {
+			chunk = chunks[i];
+			if (chunk.length > 50) {
+				if (text.length > 5) {
+					for (var j = 0, len = chunk.length; j <= len; j += 5) {
+						// save this line after the operator
+						wrapperText += chunk.substring(j, j + 5) + "\u200B";
+					}
+				} else {
+					wrapperText = chunk;
+				}
+			}
+		}
+		value = chunks.join(", ");
+	}
+	if (parameter.type == "ENUM") {
+		if (parameter.enumValues[parameter.value - 1]){
+			// save this line after the operator
+			value = parameter.enumValues[parameter.value - 1].displayName;
+		} else {
+			value = "";
+		} 
+	} else if (parameter.type == "COUNTRY") {
+		// save this line after the operator
+		value = getCountryDisplayNameByCode(parameter.value) || "";
+	} else if (parameter.type == "BOOLEAN") {
+		// save this line after the operator
+		value = (parameter.value && (parameter.value != "0") ? "%STATICTEXT_YES%" : "%STATICTEXT_NO%");
+	}
+
+	return value;
+}
